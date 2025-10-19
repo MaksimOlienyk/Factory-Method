8806c132d7ec9d0f2527b4c89c11a7423aeba31d
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 17:56:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿abstract class Creator { public abstract IProduct FactoryMethod(); }
+interface IProduct { void Operation(); }
+
+class ProductA : IProduct { public void Operation() => Console.WriteLine("ProductA created"); }
+class CreatorA : Creator { public override IProduct FactoryMethod() => new ProductA(); }
+
+class Program
+{
+    static void Main()
+    {
+        Creator creator = new CreatorA();
+        creator.FactoryMethod().Operation();
+    }
+}
